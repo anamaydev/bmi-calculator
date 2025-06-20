d1d24747999702a6b8d493313d083f9bb5543955
anamaydev <anamaydev@gmail.com>
2025-06-20 14:59:20 +0530

    feat: add header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import {Logo} from './icons/index.js'
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-center lg:justify-start">
+      <Logo className={"size-5 lg:size-8"}/>
+    </header>
+  )
+}
+export default Navbar
